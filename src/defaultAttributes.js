340921138e7ee71a5083a82482475d711d638225
CASPER\ak computer <herojaf12@gmail.com>
2023-08-07 03:48:43 +0500

    updated icons

--- src/defaultAttributes.js
+++ src/defaultAttributes.js
@@ -2,7 +2,7 @@ export default {
   xmlns: 'http://www.w3.org/2000/svg',
   width: 24,
   height: 24,
-  viewBox: '0 0 24 24',
+  viewBox: '0 0 20 20',
   fill: 'none',
   stroke: 'currentColor',
   strokeLinecap: 'round',
